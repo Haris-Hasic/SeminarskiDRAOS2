1fb4e965a736eff508105b9b5d33fd791f856886
Haris-Hasic <hhasic2@etf.unsa.ba>
2015-12-15 22:45:41 +0100

    Login Screen

--- LearningJapanese/GlavnaForma.Designer.cs
+++ LearningJapanese/GlavnaForma.Designer.cs
@@ -31,15 +31,17 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.userControl11 = new HarisKontrola.UserControl1();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.lectureMenu1 = new Lectures.LectureMenu();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.tabPage5 = new System.Windows.Forms.TabPage();
             this.tabPage6 = new System.Windows.Forms.TabPage();
             this.tabPage7 = new System.Windows.Forms.TabPage();
             this.tabPage8 = new System.Windows.Forms.TabPage();
-            this.lectureMenu1 = new Lectures.LectureMenu();
+            this.userControl21 = new HarisKontrola.UserControl2();
+            this.userControl11 = new HarisKontrola.UserControl1();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.tabPage3.SuspendLayout();
             this.SuspendLayout();
@@ -63,6 +65,7 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.userControl21);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -82,14 +85,6 @@
             this.tabPage2.Text = "Pocetna";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // userControl11
-            // 
-            this.userControl11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(202)))), ((int)(((byte)(241)))));
-            this.userControl11.Location = new System.Drawing.Point(1, 2);
-            this.userControl11.Name = "userControl11";
-            this.userControl11.Size = new System.Drawing.Size(400, 600);
-            this.userControl11.TabIndex = 0;
-            // 
             // tabPage3
             // 
             this.tabPage3.Controls.Add(this.lectureMenu1);
@@ -100,6 +95,14 @@
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Lecture";
             this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // lectureMenu1
+            // 
+            this.lectureMenu1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(202)))), ((int)(((byte)(241)))));
+            this.lectureMenu1.Location = new System.Drawing.Point(2, 2);
+            this.lectureMenu1.Name = "lectureMenu1";
+            this.lectureMenu1.Size = new System.Drawing.Size(400, 600);
+            this.lectureMenu1.TabIndex = 0;
             // 
             // tabPage4
             // 
@@ -151,13 +154,21 @@
             this.tabPage8.Text = "Help";
             this.tabPage8.UseVisualStyleBackColor = true;
             // 
-            // lectureMenu1
+            // userControl21
             // 
-            this.lectureMenu1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(202)))), ((int)(((byte)(241)))));
-            this.lectureMenu1.Location = new System.Drawing.Point(2, 2);
-            this.lectureMenu1.Name = "lectureMenu1";
-            this.lectureMenu1.Size = new System.Drawing.Size(400, 600);
-            this.lectureMenu1.TabIndex = 0;
+            this.userControl21.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(243)))));
+            this.userControl21.Location = new System.Drawing.Point(1, 2);
+            this.userControl21.Name = "userControl21";
+            this.userControl21.Size = new System.Drawing.Size(400, 600);
+            this.userControl21.TabIndex = 0;
+            // 
+            // userControl11
+            // 
+            this.userControl11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(202)))), ((int)(((byte)(241)))));
+            this.userControl11.Location = new System.Drawing.Point(1, 2);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(400, 600);
+            this.userControl11.TabIndex = 0;
             // 
             // GlavnaForma
             // 
@@ -168,6 +179,7 @@
             this.Name = "GlavnaForma";
             this.Text = "GlavnaForma";
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
             this.tabPage3.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -187,5 +199,6 @@
         private System.Windows.Forms.TabPage tabPage7;
         private System.Windows.Forms.TabPage tabPage8;
         private Lectures.LectureMenu lectureMenu1;
+        private HarisKontrola.UserControl2 userControl21;
     }
 }

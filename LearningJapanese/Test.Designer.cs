125cb2053f08ba2687037c4f165022f0ff405496
ahasanspahic <ahasanspah1@etf.unsa.ba>
2015-12-15 22:36:02 +0100

    Dodana kontrola za test.

---
+++ LearningJapanese/Test.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace LearningJapanese
+{
+    partial class Test
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button13 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.questionNumberLabel = new System.Windows.Forms.Label();
+            this.questionLabel = new System.Windows.Forms.Label();
+            this.hintLabel = new System.Windows.Forms.Label();
+            this.asnwer1Btn = new System.Windows.Forms.Button();
+            this.answer2Btn = new System.Windows.Forms.Button();
+            this.answer3Btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(202)))), ((int)(((byte)(241)))));
+            this.button13.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button13.Font = new System.Drawing.Font("Berlin Sans FB", 42F);
+            this.button13.ForeColor = System.Drawing.Color.White;
+            this.button13.Location = new System.Drawing.Point(35, 500);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(85, 70);
+            this.button13.TabIndex = 20;
+            this.button13.Text = "<";
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(202)))), ((int)(((byte)(241)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Berlin Sans FB", 42F);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(280, 500);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(85, 70);
+            this.button1.TabIndex = 21;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // questionNumberLabel
+            // 
+            this.questionNumberLabel.AutoSize = true;
+            this.questionNumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.questionNumberLabel.Location = new System.Drawing.Point(125, 50);
+            this.questionNumberLabel.Name = "questionNumberLabel";
+            this.questionNumberLabel.Size = new System.Drawing.Size(123, 26);
+            this.questionNumberLabel.TabIndex = 22;
+            this.questionNumberLabel.Text = "Question 1:";
+            // 
+            // questionLabel
+            // 
+            this.questionLabel.AutoSize = true;
+            this.questionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.questionLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.questionLabel.Location = new System.Drawing.Point(51, 100);
+            this.questionLabel.Name = "questionLabel";
+            this.questionLabel.Size = new System.Drawing.Size(314, 31);
+            this.questionLabel.TabIndex = 23;
+            this.questionLabel.Text = "First question on Test?";
+            // 
+            // hintLabel
+            // 
+            this.hintLabel.AutoSize = true;
+            this.hintLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.hintLabel.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.hintLabel.Location = new System.Drawing.Point(87, 154);
+            this.hintLabel.Name = "hintLabel";
+            this.hintLabel.Size = new System.Drawing.Size(231, 26);
+            this.hintLabel.TabIndex = 24;
+            this.hintLabel.Text = "( Choose one answer )";
+            // 
+            // asnwer1Btn
+            // 
+            this.asnwer1Btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(202)))), ((int)(((byte)(241)))));
+            this.asnwer1Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.asnwer1Btn.Font = new System.Drawing.Font("Berlin Sans FB", 22F);
+            this.asnwer1Btn.ForeColor = System.Drawing.Color.White;
+            this.asnwer1Btn.Location = new System.Drawing.Point(92, 212);
+            this.asnwer1Btn.Name = "asnwer1Btn";
+            this.asnwer1Btn.Size = new System.Drawing.Size(214, 69);
+            this.asnwer1Btn.TabIndex = 25;
+            this.asnwer1Btn.Text = "Story";
+            this.asnwer1Btn.UseVisualStyleBackColor = false;
+            // 
+            // answer2Btn
+            // 
+            this.answer2Btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(202)))), ((int)(((byte)(241)))));
+            this.answer2Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.answer2Btn.Font = new System.Drawing.Font("Berlin Sans FB", 22F);
+            this.answer2Btn.ForeColor = System.Drawing.Color.White;
+            this.answer2Btn.Location = new System.Drawing.Point(92, 314);
+            this.answer2Btn.Name = "answer2Btn";
+            this.answer2Btn.Size = new System.Drawing.Size(214, 69);
+            this.answer2Btn.TabIndex = 26;
+            this.answer2Btn.Text = "Story";
+            this.answer2Btn.UseVisualStyleBackColor = false;
+            // 
+            // answer3Btn
+            // 
+            this.answer3Btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(202)))), ((int)(((byte)(241)))));
+            this.answer3Btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.answer3Btn.Font = new System.Drawing.Font("Berlin Sans FB", 22F);
+            this.answer3Btn.ForeColor = System.Drawing.Color.White;
+            this.answer3Btn.Location = new System.Drawing.Point(104, 406);
+            this.answer3Btn.Name = "answer3Btn";
+            this.answer3Btn.Size = new System.Drawing.Size(214, 69);
+            this.answer3Btn.TabIndex = 27;
+            this.answer3Btn.Text = "Story";
+            this.answer3Btn.UseVisualStyleBackColor = false;
+            // 
+            // Test
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(202)))), ((int)(((byte)(241)))));
+            this.Controls.Add(this.answer3Btn);
+            this.Controls.Add(this.answer2Btn);
+            this.Controls.Add(this.asnwer1Btn);
+            this.Controls.Add(this.hintLabel);
+            this.Controls.Add(this.questionLabel);
+            this.Controls.Add(this.questionNumberLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button13);
+            this.Name = "Test";
+            this.Size = new System.Drawing.Size(400, 600);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label questionNumberLabel;
+        private System.Windows.Forms.Label questionLabel;
+        private System.Windows.Forms.Label hintLabel;
+        private System.Windows.Forms.Button asnwer1Btn;
+        private System.Windows.Forms.Button answer2Btn;
+        private System.Windows.Forms.Button answer3Btn;
+    }
+}
---
+++ LearningJapanese/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LearningJapanese
+{
+    public partial class Test : UserControl
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
